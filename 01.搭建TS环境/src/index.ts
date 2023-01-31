09fe7223bf6422da73090776d7a44a72ed2467b0
dajiangjunok <779503577@qq.com>
2023-01-31 23:37:15 +0800

    feat 搭建TS

---
+++ 01.搭建TS环境/src/index.ts
@@ -0,0 +1,20 @@
+// console.log('Hello TypeScript!');
+let a = 1 + 2
+let b = a + 3
+let c = {
+  apple: a,
+  banana: b
+}
+let d = c.apple * 4
+
+let e = a.toString() + c.toString()
+
+let obj:{ name:string,age:number} = {
+  name:'Tom',
+  age:18
+}
+
+function add(a:number,b:number){
+  return a + b
+}
+ 
